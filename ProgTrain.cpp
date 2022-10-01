a8f60e3a8734a029e8805c4560988799df0bce3e
Zeng Shenggen <sg_zeng@sohu.com>
2022-10-01 08:26:17 -0400

    create base file

---
+++ ProgTrain.cpp
@@ -0,0 +1,9 @@
+// ProgTrain.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "testcase.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
