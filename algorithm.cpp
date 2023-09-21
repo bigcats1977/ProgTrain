1088ffe3d9f9f9c6bdecf538d947424caf71cc3d
Zeng Shenggen <sg_zeng@sohu.com>
2023-09-21 00:08:03 -0400

    4. Median of Two Sorted Arrays

--- algorithm.cpp
+++ algorithm.cpp
@@ -90,6 +90,44 @@ int lengthOfLongestSubstring(char* s)
     return max;
 }
 
+// 4. Median of Two Sorted Arrays
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size)
+{
+    if (nums1Size > nums2Size)
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+
+    int m = nums1Size;
+    int n = nums2Size;
+    int left = 0, right = m;
+
+    while (left <= right) {
+        int partition1 = (left + right) / 2;
+        int partition2 = (m + n + 1) / 2 - partition1;
+
+        int maxLeft1 = (partition1 == 0) ? INT_MIN : nums1[partition1 - 1];
+        int minRight1 = (partition1 == m) ? INT_MAX : nums1[partition1];
+
+        int maxLeft2 = (partition2 == 0) ? INT_MIN : nums2[partition2 - 1];
+        int minRight2 = (partition2 == n) ? INT_MAX : nums2[partition2];
+
+        if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+            if ((m + n) % 2 == 0) {
+                return (double)(fmax(maxLeft1, maxLeft2) + fmin(minRight1, minRight2)) / 2;
+            }
+            else {
+                return (double)fmax(maxLeft1, maxLeft2);
+            }
+        }
+        else if (maxLeft1 > minRight2) {
+            right = partition1 - 1;
+        }
+        else {
+            left = partition1 + 1;
+        }
+    }
+    return 0;
+}
+
 // 5. Longest Palindromic Substring
 int maxpalstr(char* s, int l, int r)
 {
