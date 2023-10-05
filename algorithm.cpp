bab9fe2ac5da6dc38369969a4e704f92ea458ff6
SGZeng <sg_zeng@sohu.com>
2023-10-05 14:17:17 -0400

    706. Design HashMap

--- algorithm.cpp
+++ algorithm.cpp
@@ -6675,7 +6675,22 @@ int search(int* nums, int numsSize, int target) {
 
 // 706. Design HashMap
 // myself HashMap
-
+class MyHashMap {
+public:
+    int arr[1000001];
+    MyHashMap() {
+        fill(arr, arr + 1000000, -1); // initalizing with -1.
+    }
+    void put(int key, int val) {
+        arr[key] = val;
+    }
+    int get(int key) {
+        return arr[key];
+    }
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
 
 // 707. Design Linked List
 // MyLinkedList
