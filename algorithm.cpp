7c1903292261a06e07e170791700d9a500700943
SGZeng <sg_zeng@sohu.com>
2023-11-14 17:26:30 -0500

    221. Maximal Square

--- algorithm.cpp
+++ algorithm.cpp
@@ -5090,9 +5090,28 @@ bool containsNearbyDuplicate(vector<int>& nums, int k)
 int maximalSquare(char** matrix, int matrixSize, int* matrixColSize)
 {
     int m = matrixSize, n = matrixColSize[0];
-    int i, j, k;
+    int i, j;// , k;
     int maxval = 0;
     int** dp = (int**)malloc(m * sizeof(int*));
+    for (i = 0; i < m; i++) {
+        dp[i] = (int*)calloc(n, sizeof(int));
+    }
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            if (matrix[i][j] == '1') {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 1;
+                else {
+                    int min = dp[i - 1][j - 1] > dp[i][j - 1] ? dp[i][j - 1] : dp[i - 1][j - 1];
+                    dp[i][j] = (min > dp[i - 1][j] ? dp[i - 1][j] : min) + 1;
+                }
+                maxval = maxval < dp[i][j] ? dp[i][j] : maxval;
+                printf("%d,%d: %d\r\n", i, j, dp[i][j]);
+            }
+        }
+        printf("\r\n");
+    }
+#if 0
     for (i = 0; i < m; i++) {
         dp[i] = (int*)malloc(n * sizeof(int));
         dp[i][0] = matrix[i][0] - '0';
@@ -5130,7 +5149,7 @@ int maximalSquare(char** matrix, int matrixSize, int* matrixColSize)
             maxval = maxval < dp[i][j] ? dp[i][j] : maxval;
         }
     }
-
+#endif
     for (i = 0; i < m; i++)
         free(dp[i]);
     free(dp);
